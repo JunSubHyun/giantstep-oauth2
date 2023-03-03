084650fa369883872b27c02c459a1157de7d1531
JunSubHyun <guswnstjq10@gmail.com>
2023-03-03 15:31:17 +0900

    Initial commit

---
+++ ShareObject/src/main/java/shareObject/ShareObjectApplication.java
@@ -0,0 +1,12 @@
+package shareObject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareObjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareObjectApplication.class, args);
+    }
+
+}
